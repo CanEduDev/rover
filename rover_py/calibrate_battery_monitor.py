16425989cb724b531c869c564f485c66365a9b4f
Hashem Hashem <hashem@canedudev.com>
2025-04-25 09:26:53 +0200

    chore: update battery monitor calibration script
    Add another instruction about connecting the board to the CAN bus.

--- rover_py/calibrate_battery_monitor.py
+++ rover_py/calibrate_battery_monitor.py
@@ -8,7 +8,8 @@ from rover import Envelope, battery
 def main():
     calibration_voltage_mv = 24_000
     input(
-        f"""Connect battery monitor board to DC power supply with {calibration_voltage_mv / 1000}V on all cells.
+        f"""Connect power board to DC power supply with {calibration_voltage_mv / 1000}V on all cells.
+Connect CAN interface to power board. Make sure the connection is terminated with a 120 ohm resistance on at least one end.
 Turn power on and press Enter to continue.
     """
     )
