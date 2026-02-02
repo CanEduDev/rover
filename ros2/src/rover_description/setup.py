55bb7686e8354f8e33bfbd95b39ca902b8e75bf3
Hashem Hashem <hashem@canedudev.com>
2026-02-02 16:57:15 +0100

    fix(ros2): URDF package linting and formatting

--- ros2/src/rover_description/setup.py
+++ ros2/src/rover_description/setup.py
@@ -1,41 +1,43 @@
-from setuptools import setup
-from glob import glob
-import os
+from pathlib import Path
 
-package_name = 'rover_description'
+from setuptools import setup
+
+package_name = "rover_description"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
         # ament index
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         # package.xml
-        ('share/' + package_name, ['package.xml']),
-
+        ("share/" + package_name, ["package.xml"]),
         # URDF / Xacro
-        (os.path.join('share', package_name, 'urdf'),
-            glob('urdf/*')),
-
+        (
+            str(Path("share") / package_name / "urdf"),
+            [str(p) for p in Path("urdf").glob("*")],
+        ),
         # Meshes (recursive)
-        (os.path.join('share', package_name, 'meshes'),
-            glob('meshes/**/*', recursive=True)),
-
+        (
+            str(Path("share") / package_name / "meshes"),
+            [str(p) for p in Path("meshes").rglob("*") if p.is_file()],
+        ),
         # sensors (recursive)
-        (os.path.join('share', package_name, 'sensors'),
-            glob('sensors/**/*', recursive=True)),
-
+        (
+            str(Path("share") / package_name / "sensors"),
+            [str(p) for p in Path("sensors").rglob("*") if p.is_file()],
+        ),
         # Static config (unused for now)
-        (os.path.join('share', package_name, 'config'),
-            glob('config/*')),
+        (
+            str(Path("share") / package_name / "config"),
+            [str(p) for p in Path("config").glob("*")],
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='None',
-    maintainer_email='none@todo.todo',
-    description='Robot description (URDF/Xacro) for rover',
-    license='Apache-2.0',
+    maintainer="None",
+    maintainer_email="none@todo.todo",
+    description="Robot description (URDF/Xacro) for rover",
+    license="Apache-2.0",
 )
